57dd952c4d9de3f16cc7f3d94d30787bfc24dae6
diamkil <35732047+diamkil@users.noreply.github.com>
2020-08-12 15:31:17 -0400

    Updated the token system so that's it stays safe

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PandacraftsBot
 Our discord bot
+
+##To use it you will need to do the following
+1. Copy token-exemple.json to token.json and put in your own bot token
+2. run "npm install" to install dependencies
+3. run "npm start" to start the bot
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const Discord = require('discord.js');
+const token = require('./token.json');
 const client = new Discord.Client();
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.login('NTg3NzQ0MjA1ODEyNjYyNDY2.XP7Bcg.dKz8ebmDm6tlLJj9qgSDTfkmbqo');
+client.login('token.token');
