b458b13d4e29480d390483c74367709a9e5f0373
diamkil <35732047+diamkil@users.noreply.github.com>
2020-08-12 18:55:45 -0400

    Renamed the original command variable to commandName (to be descriptive about what it actually is, and so that you can name one of our variables as command later). Changed the following if statement appropriately. Made a variable named command which is the actual command object. Changed the line inside the try/catch statement to use the command variable instead.

--- index.js
+++ index.js
@@ -1,30 +1,48 @@
 // require the discord.js module
 const Discord = require('discord.js');
+// require node's filesystem module
+const fs = require('fs');
 // require the config file
 const config = require('./config.json');
 // require the token file
 const { token } = require('./token.json');
 // create the discord client
 const client = new Discord.Client();
+// create a collection for the commands
+client.commands = new Discord.Collection();
+// find all .js files in commands folder
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
+
+// require every .js files the commandFiles function gives
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
+}
 
 // Command handler
 client.on('message', message => {
 	if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
 	const args = message.content.slice(config.prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
+	const commandName = args.shift().toLowerCase();
+	if (!client.commands.has(commandName)) return;
 
-	if (command === 'ping') {
-		message.channel.send('Pong.');
+	const command = client.commands.get(commandName);
+
+	try {
+		command.execute(message, args);
 	}
-	else if (command === 'beep') {
-		message.channel.send('Boop.');
+	catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
 	}
-	// other commands...
 });
 
 // log in using the token in token.json
