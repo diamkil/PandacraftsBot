85c84852e63418ce142d89b3e3f7d9141e878d63
diamkil <35732047+diamkil@users.noreply.github.com>
2020-08-12 15:26:12 -0400

    Started the actual bot code

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login('NTg3NzQ0MjA1ODEyNjYyNDY2.XP7Bcg.dKz8ebmDm6tlLJj9qgSDTfkmbqo');
