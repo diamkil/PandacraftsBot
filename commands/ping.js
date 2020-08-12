630f2b505728ec4b3e1a0f0fd297409e03a4372d
diamkil <35732047+diamkil@users.noreply.github.com>
2020-08-12 19:32:14 -0400

    Updated loads of stuff

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,11 @@
 module.exports = {
 	name: 'ping',
 	description: 'Ping!',
+	args: false,
+	usage: '',
+	guildOnly: false,
+	cooldown: 5,
+	aliases: ['pong'],
 	execute(message, args) {
 		message.channel.send('Pong.');
 	},
--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 // find all .js files in commands folder
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+// creates a collection for the cooldowns
+const cooldowns = new Discord.Collection();
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -32,10 +34,40 @@ client.on('message', message => {
 
 	const args = message.content.slice(config.prefix.length).trim().split(/ +/);
 	const commandName = args.shift().toLowerCase();
-	if (!client.commands.has(commandName)) return;
+	const command = client.commands.get(commandName)
+		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
-	const command = client.commands.get(commandName);
+	if (!command) return;
+	if (command.guildOnly && message.channel.type === 'dm') {
+		return message.reply('I can\'t execute that command inside DMs!');
+	}
+	if (command.args && !args.length) {
+		let reply = `You didn't provide any arguments, ${message.author}!`;
 
+		if (command.usage) {
+			reply += `\nThe proper usage would be: \`${config.prefix}${command.name} ${command.usage}\``;
+		}
+
+		return message.channel.send(reply);
+	}
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
+	}
+
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 3) * 1000;
+
+	if (timestamps.has(message.author.id)) {
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+		}
+	}
+	timestamps.set(message.author.id, now);
+	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 	try {
 		command.execute(message, args);
 	}
