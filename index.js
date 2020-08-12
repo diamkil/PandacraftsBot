72f7fccecc507899012ea39ea4868dbc9e6ce924
diamkil <35732047+diamkil@users.noreply.github.com>
2020-08-12 15:51:15 -0400

    added some stuff

--- index.js
+++ index.js
@@ -15,10 +15,13 @@ client.once('ready', () => {
 client.on('message', message => {
 	console.log(message.content);
   // reply to !ping by "pong"
-  if (message.content === '!ping') {
+  if (message.content.startsWith(`${config.prefix}ping`)) {
   	// send back "Pong." to the channel the message was sent in
   	message.channel.send('Pong.');
   }
+	else if (message.content === `${config.prefix}server-name`) {
+	message.channel.send(`This server's name is: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+	}
 });
 
 // log in using the token in token.json
