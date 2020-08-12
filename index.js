729c58b770adc13e5c32b97ce40a832a0bc2cd80
diamkil <35732047+diamkil@users.noreply.github.com>
2020-08-12 15:36:11 -0400

    added comments and the log messages function

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+// require the discord.js module
 const Discord = require('discord.js');
+// require the token file
 const token = require('./token.json');
+// create the discord client
 const client = new Discord.Client();
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
+// logs every message the bot received
+client.on('message', message => {
+	console.log(message.content);
+});
+
+// log in using the token in token.json
 client.login(token.token);
